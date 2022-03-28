bc00bfc3db6d7a826d519d4733ce4c1cefbe411c
Irina <irina.khvorova99@gmail.com>
2022-03-28 21:23:35 +0300

    Созданы классы последовательность и ДНК, исправлена строка по сохранению последовательности в словаре, теперь можно хранить и имя последовательности

--- primer_search.py
+++ primer_search.py
@@ -1,3 +1,43 @@
+class Sequence:
+
+    alphabet = []
+
+    def __init__(self, name, seq):
+        self.name = name
+        self.seq = seq
+
+    def __getitem__(self, item):  # возвращает по запросу символ в последовательности
+        return self.seq[item]
+
+    def __len__(self):  # возвращает длину последовательности
+        return len(self.seq)
+
+    def name(self):  # возвращает имя последовательности
+        return self.name
+
+    @property
+    def sequence(self):  # возвращает саму последовательность
+        return self.seq
+
+    @property
+    def len(self):
+        return len(self.seq)  # возвращает длину последовательности
+
+class DNA(Sequence):
+
+    alphabet = ['A', 'T', 'G', 'C']
+
+    def __init__(self, name, seq):
+        super().__init__(name, seq)
+
+    def complement(self):  # возвращает комплементарную последовательность
+        comp_seq = ''
+        comp = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+        for i in self.seq:
+            comp_seq += comp[i]
+        return comp_seq
+
+
 # Обработка данных, введенных пользователем
 def input_file_process(list_path, table_path):
     seqs = {}  # создали пустой словарь, чтобы добавить туда все последовательности
@@ -7,7 +47,7 @@ def input_file_process(list_path, table_path):
                 seqs['# имя группы #'] = []  # если нет, то создем ключ с именем группы
             # добавляем последовательность в словарь, надо вытащить из списка
             with open(list_path) as list:
-                seqs['# имя группы #'].append('# сама последовательность из списка от пользователя #')
+                seqs['# имя группы #'].append(DNA('# имя последовательности #', '# сама последовательность #'))
 
 
 if __name__ == '__main__':
