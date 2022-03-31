f0f8aa10473b1832c1f8297be9f38c776a109181
Irina <irina.khvorova99@gmail.com>
2022-03-31 11:46:37 +0300

    Исправлена функция по созданию словаря k-меров, добавлена функция по созданию словаря праймеров

--- primer_search.py
+++ primer_search.py
@@ -42,52 +42,77 @@ class DNA(Sequence):
 # Обработка данных, введенных пользователем
 def input_file_process(table_path, list_path):
     seqs = {}  # создали пустой словарь, чтобы добавить туда все последовательности
-    with open(table_path, 'r') as table:  # открываем файл на чтение
+    with open(table_path, 'r') as table:  # открываем таблицу на чтение
         table = csv.reader(table, delimiter=';')
         next(table, None)   # пропускаем 1 строчку таблицы (названия столбцов)
         for row in table:  # пробегаемся по всей таблице, что ввел пользователь
             if row[0] not in seqs:  # проверяем наличие группы в словаре, надо вытащить из таблицы
                 seqs[row[0]] = []  # если нет, то создем ключ с именем группы и к нему пустой список
             # добавляем последовательность в словарь
-            with open(list_path, 'r') as list:
+            with open(list_path, 'r') as list:  # открываем список на чтение
                 list = SeqIO.parse(list, 'fasta')  # преобразуем fasta-файл для вычленения интересующих пар-в
                 for feature in list:  # для каждой посл-ти из списка ищем совпадение имени в списке и табл
                     if feature.name in row:
                         seqs[row[0]].append(DNA(str(feature.name), str(feature.seq)))
 
-
-# код для того, чтобы посмотреть наш словарь, потом уберем
+    # код для того, чтобы посмотреть словарь исходных последовательностей
     for key in seqs:
         for s in seqs[key]:
             print(key, s.name, s.seq, len(s)) # выводит группу, имя, последовательность и ее длину
+
     return seqs
+
 
 # функция для разделения последовательности на k-меры
 def seq_kmers(k, sequence):  # принимает длину к-мера и последовательность
-    kmers = []
-    for s in range(0, len(sequence) - k + 1):
-        kmer = sequence[s:s+k]
-        kmers.append(kmer)
+    kmers = []  # создаем пустой список k-меров
+    for s in range(0, len(sequence) - k + 1):  # пробегаемся по всей последовательности
+        kmers.append(sequence[s:s+k])  # делаем срезы длины k и добавляем их в список
     return kmers   # возвращает списком все к-меры последовательности
 
-# создание словаря, где каждой возможной длине праймера в группах соответствуют пары
-# (последовательность : множество подстрок длины k)
-def kmers_dict(dict):
+
+# создание словаря k-меров
+# в группах для каждой возможной длины праймера хранится список множеств
+# в множествах находятся подстроки длины k для каждой последовательности группы
+def kmers_dict(seq_dict):
     kmers_dict = {}  # создали пустой словарь k-меров
-    for group in dict:  # для каждой группы в словаре
+    for group in seq_dict:  # для каждой группы в словаре
         kmers_dict[group] = {}  # словарь состоит из групп
-        for s in dict[group]:  # для каждой последовательности в группе
+        for s in seq_dict[group]:  # для каждой последовательности в группе
             for k in range(16, 31):  # для каждой длины праймерв (задаем границы)
-                kmers_dict[group][k] = {}  # в группах возможные варианты длины k-меров
-                unique_kmers = set(seq_kmers(k, s.seq))  # находим множество к-меров (длины k)
-                kmers_dict[group][k][s] = unique_kmers  # в k хранятся (последовательность : подстроки длины k)
-    print(kmers_dict)
+                if k not in kmers_dict[group]:  # проверка на наличие ключа - длины k
+                    kmers_dict[group][k] = []  # если нет, то создаем ключ и пустой список для множеств
+                unique_kmers = set(seq_kmers(k, s.seq))  # находим множество уникальных к-меров (длины k)
+                kmers_dict[group][k].append(unique_kmers)  # добавляем в список множество с подстроками длины k
+    return kmers_dict  # возвращаем словарь k-меров
+
+
+# создание словаря праймеров
+# оставляем из словаря k-меров только те праймеры, которые общие для всех последовательностей группы
+def primer_dict(seq_dict):
+    primer_dict = {}  # сойдаем пустой словарь праймеров
+    dict = kmers_dict(seq_dict)  # получаем словарь k-меров
+    for group in dict:  # для каждой группы
+        primer_dict[group] = []  # словарь праймеров состоит из групп
+        for k in dict[group]:  # для каждой длины праймерв в группе
+            for i in range(len(dict[group][k])-1):  # пробегаемся по всем множествам k-меров в группе
+                if i == 0:  # проверка на то, что это первое множество
+                    k_intersection = dict[group][k][i]  # в пересечении будет первое мноество
+                k_intersection.intersection_update(dict[group][k][i+1])  # находим пересечение всех множеств списка
+            for primer in k_intersection:  # для каждого праймера в множестве пересейчений
+                primer_dict[group].append(primer)  # добавляем праймер в словарь (группа : список праймеров)
+
+    # код для того, чтобы посмотреть словарь праймеров
+    for key in primer_dict:  # для каждой группы
+        print(key, primer_dict[key]) # выводит группу и список праймеров
+
 
 if __name__ == '__main__':
     # Пользователь вводит полные пути к файлам
-    path_to_table, path_to_list = input('Введите путь до таблицы '), input('Введите путь до списка ')
-    # /Users/akhvorov/Desktop/home_task/BioProject/TestPro.csv
-    # /Users/akhvorov/Desktop/home_task/BioProject/TestPro.fasta
-    seq_dict = input_file_process(path_to_table, path_to_list)
-    kmers_dict(seq_dict)
+    #path_to_table, path_to_list = input('Введите путь до таблицы '), input('Введите путь до списка ')
 
+    path_to_table = '/Users/akhvorov/Desktop/home_task/BioProject/TestPro.csv'
+    path_to_list = '/Users/akhvorov/Desktop/home_task/BioProject/TestPro.fasta'
+
+    seq_dict = input_file_process(path_to_table, path_to_list)
+    primer_dict(seq_dict)
