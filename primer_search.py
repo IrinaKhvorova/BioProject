a588e38f751b4aeeadf629eb6162f4d6499ea1c6
Irina <irina.khvorova99@gmail.com>
2022-04-13 16:03:53 +0300

    Создание матричной и комплементарной последовательности выделено обратно в функцию

--- primer_search.py
+++ primer_search.py
@@ -67,6 +67,13 @@ class Primer(DNA):
         temper = 22 + 1.46 * (2*(g+c) + (a+t))
         return temper
 
+# функция для создания двух цепей ДНК
+def double_dna(name, seq):
+    matrix_seq = DNA(name + " matr", str(seq))  # создает матричную последовательность
+    comp_seq = DNA(name, matrix_seq.complement())  # создает комплементарную последовательность
+    rev_seq = DNA(name + " comp", comp_seq.reverse())  # создает обратную последовательность
+    return matrix_seq, rev_seq
+
 # обработка данных, введенных пользователем
 def input_file_process(table_path, list_path):
     seqs = {}  # создали пустой словарь, чтобы добавить туда все последовательности
@@ -81,10 +88,7 @@ def input_file_process(table_path, list_path):
                 list = SeqIO.parse(list, 'fasta')  # преобразуем fasta-файл для вычленения интересующих пар-в
                 for feature in list:  # для каждой посл-ти из списка ищем совпадение имени в списке и табл
                     if feature.name in row:
-                        # seqs[row[0]].append(DNA(str(feature.name), str(feature.seq)))
-                        matrix_seq = DNA(str(feature.name) + " matr", str(feature.seq))  # создает матричную последовательность
-                        comp_seq = DNA(str(feature.name), matrix_seq.complement())  # создает комплементарную последовательность
-                        rev_seq = DNA(comp_seq.name + " comp", comp_seq.reverse())  # создает обратную последовательность
+                        matrix_seq, rev_seq = double_dna(str(feature.name), str(feature.seq))
                         seqs[row[0]].append(matrix_seq)
                         seqs[row[0]].append(rev_seq)
 
@@ -95,81 +99,11 @@ def input_file_process(table_path, list_path):
 
     return seqs
 
-
-# функция для создания двух цепей ДНК
-# def double_dna(seqs):
-#     for group in seqs:              # для каждой группы
-#         double_seqs = []            # создается пустой словарь
-#         for orig_seq in seqs[group]:      # для каждой последовательности
-#             matr_seq = DNA(orig_seq.name + " matr", orig_seq.seq)
-#             comp_seq = DNA(orig_seq.name, orig_seq.complement())    # создает комплементарную последовательность
-#             rev_seq = DNA(orig_seq.name + " comp", comp_seq.reverse())        # создает обратную последовательность
-#
-#             double_seqs.append(matr_seq)       # добавляем оригинальный сиквенс в словарь
-#             double_seqs.append(rev_seq)        # добавляем новый сиквенс в словарь
-#
-#         seqs[group] = double_seqs     # перезаписываем словарь
-#
-#     # код для того, чтобы посмотреть словарь двух последовательностей
-#     for key in seqs:
-#         for s in seqs[key]:
-#             print(key, s.name, s.seq, len(s))  # выводит группу, имя, последовательность и ее длину
-#
-#     return seqs
-
-# # функция для разделения последовательности на k-меры
-# def seq_kmers(k, sequence):  # принимает длину к-мера и последовательность
-#     kmers = []  # создаем пустой список k-меров
-#     for s in range(0, len(sequence) - k + 1):  # пробегаемся по всей последовательности
-#         kmers.append(sequence[s:s+k])  # делаем срезы длины k и добавляем их в список
-#     return kmers   # возвращает списком все к-меры последовательности
-
-
-# создание словаря k-меров
-# в группах для каждой возможной длины праймера хранится список множеств
-# в множествах находятся подстроки длины k для каждой последовательности группы
-# def kmers_dict(seq_dict):
-#     kmers_dict = {}  # создали пустой словарь k-меров
-#     for group in seq_dict:  # для каждой группы в словаре
-#         kmers_dict[group] = {}  # словарь состоит из групп
-#         for s in seq_dict[group]:  # для каждой последовательности в группе
-#             for k in range(16, 31):  # для каждой длины праймерв (задаем границы)
-#                 if k not in kmers_dict[group]:  # проверка на наличие ключа - длины k
-#                     kmers_dict[group][k] = []  # если нет, то создаем ключ и пустой список для множеств
-#                 unique_kmers = set(seq_kmers(k, s.seq))  # находим множество уникальных к-меров (длины k)
-#                 kmers_dict[group][k].append(unique_kmers)  # добавляем в список множество с подстроками длины k
-#     return kmers_dict  # возвращаем словарь k-меров
-
-
-# создание словаря праймеров
-# оставляем из словаря k-меров только те праймеры, которые общие для всех последовательностей группы
-# def prer_primer_dict(seq_dict):
-#     primer_dict = {}  # создаем пустой словарь праймеров
-#     dict = kmers_dict(seq_dict)  # получаем словарь k-меров
-#     for group in dict:  # для каждой группы
-#         primer_dict[group] = []  # словарь праймеров состоит из групп
-#         for k in dict[group]:  # для каждой длины праймерв в группе
-#             for i in range(len(dict[group][k])-1):  # пробегаемся по всем множествам k-меров в группе
-#                 if i == 0:  # проверка на то, что это первое множество
-#                     k_intersection = dict[group][k][i]  # в пересечении будет первое мноество
-#                 k_intersection.intersection_update(dict[group][k][i+1])  # находим пересечение всех множеств списка
-#             for primer in k_intersection:  # для каждого праймера в множестве пересейчений
-#                 primer_dict[group].append(primer)  # добавляем праймер в словарь (группа : список праймеров)
-#
-#     # код для того, чтобы посмотреть словарь праймеров
-#     print('prer_primer_dict')
-#     for group in primer_dict:  # для каждой группы
-#         print(group, primer_dict[group]) # выводит группу и список праймеров
-#
-#     return primer_dict   # возвращаем словарь праймеров
-
 def pre_primer_search(seq_list):
-    seqs_count = len(seq_list)  # количество последовательностей
-    seq_length = len(seq_list[0])  # длина последовательности
     pre_primer_set, pre_primer = set(), ''  # пустое множество для пре-праймеров и нустой препраймер
-    for i in range(seq_length):  # проходимся по всем буквам в последовательности
+    for i in range(len(seq_list[0])):  # проходимся по всем буквам в последовательности
         letter_check = True  # изначально считаем, у всех последовательностей одинаковый i-ый нуклеотид
-        for j in range(0, seqs_count-2, 2):  # пробегаемся по всем последовательностям
+        for j in range(0, len(seq_list)-2, 2):  # пробегаемся по всем последовательностям
             # print(seq_list[j][i], '?=?', seq_list[j+1][i])
             if seq_list[j][i] != seq_list[j+2][i]:  # проверка на равенство нуклеотидов
                 letter_check = False  # выявляем разницу в нуклеотидах
@@ -181,7 +115,7 @@ def pre_primer_search(seq_list):
             pre_primer += seq_list[0][i]  # записываем нуклеотид в пре-праймер
     return pre_primer_set  # возвращаем множество препраймеров
 
-def pre_primer_dict(seq_dict):
+def primer_dict(seq_dict):
     print('pre_primer_dict')
     pre_primer_dict = {}  # создали пустой словарь для препраймеров
     for group in seq_dict:  # для каждой группы в словаре последовательностей
@@ -389,7 +323,7 @@ if __name__ == '__main__':
     # path_to_list = '/Users/dnayd/Desktop/Project/TestPro.fasta'
 
     seq_dict = input_file_process(path_to_table, path_to_list)   # словарь исходных последовательностей
-    pre_primer_dict = pre_primer_dict(seq_dict)
+    pre_primer_dict = primer_dict(seq_dict)
     # prer_primers_dict = prer_primer_dict(seq_dict)                 # словарь сходных участков последовательностей внутри групп
     # unique_pre_primers = unique_primer(prer_primers_dict)         # словарь сходных участков без повторов и подстрок
     # forw_rev_primers = forw_rev_primers(unique_pre_primers)     # словарь прямых и обратных праймеров
